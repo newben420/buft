08848d1987eacd2bc71c2006256083dbc8da7128
newben420 <wraith@wraith.wraith>
2025-04-15 13:24:02 +0100

    working on analysis - lunch break

--- engine/analysis.js
+++ engine/analysis.js
@@ -1,7 +1,15 @@
+const { compute1ExpDirection, computeArithmeticDirection } = require("../lib/direction");
 const Log = require("../lib/log");
 const Candlestick = require("../model/candlestick")
 const Signal = require("../model/signal");
 const Site = require("../site");
+const {
+    MACD, PSAR, Stochastic, bullish, bearish, VWAP, ADL, ATR, AwesomeOscillator, ROC, ForceIndex,
+    TRIX, ADX, BollingerBands, CCI, MFI, RSI, abandonedbaby, bearishengulfingpattern, darkcloudcover,
+    piercingline, eveningstar, eveningdojistar, threeblackcrows, gravestonedoji, bearishharami, bearishmarubozu,
+    tweezertop, hangingman, shootingstar, bearishharamicross, morningstar, threewhitesoldiers, bullishengulfingpattern,
+    morningdojistar, hammerpattern, dragonflydoji, bullishharami, bullishmarubozu, bullishharamicross, tweezerbottom,
+} = require("technicalindicators");
 
 /**
  * This analysis candlestick data and generates entry signals
@@ -18,7 +26,53 @@ class Analysis {
         return new Promise((resolve, reject) => {
             Log.flow(`Analysis > ${symbol} > Initialized.`, 5);
             if (data.length >= Site.AS_MIN_ROWS) {
-                // TODO - run analysis
+                const open = data.map(x => x.open);
+                const high = data.map(x => x.high);
+                const low = data.map(x => x.low);
+                const close = data.map(x => x.close);
+                const volume = data.map(x => x.volume);
+                const latestRate = close[close.length - 1] || 0;
+                const csd = { open, close, high, low };
+
+                const priceDir = computeArithmeticDirection(close);
+
+
+                // PRIMARY INDICATORS
+                const macd = MACD.calculate({ values: close, fastPeriod: Site.IN_MACD_FAST_PERIOD, slowPeriod: Site.IN_MACD_SLOW_PERIOD, signalPeriod: Site.IN_MACD_SIGNAL_PERIOD, SimpleMAOscillator: false, SimpleMASignal: false });
+                const psar = PSAR.calculate({ high, low, step: Site.IN_PSAR_STEP, max: Site.IN_PSAR_MAX });
+                const stoch = Stochastic.calculate({ close, high, low, period: Site.IN_STOCH_PERIOD, signalPeriod: Site.IN_STOCH_SIGNAL_PERIOD });
+                // PRIMARY COMPUTATIONS
+                const macdBull = macd.length > 0 ? (((macd[macd.length - 1].MACD || macd[macd.length - 1].MACD === 0) && (macd[macd.length - 1].signal || macd[macd.length - 1].signal === 0)) ? macd[macd.length - 1].MACD > macd[macd.length - 1].signal : false) : false;
+                const macdBear = macd.length > 0 ? (((macd[macd.length - 1].MACD || macd[macd.length - 1].MACD === 0) && (macd[macd.length - 1].signal || macd[macd.length - 1].signal === 0)) ? macd[macd.length - 1].MACD < macd[macd.length - 1].signal : false) : false;
+                const psarBull = (psar[psar.length - 1] ?? latestRate) < latestRate;
+                const psarBear = (psar[psar.length - 1] ?? latestRate) > latestRate;
+                const stochOB = stoch.length > 0 ? (Math.max(stoch[stoch.length - 1].k, stoch[stoch.length - 1].d) > 80) : false;
+                const stochOS = stoch.length > 0 ? (Math.max(stoch[stoch.length - 1].k, stoch[stoch.length - 1].d) < 20) : false;
+                const stochBull = stochOB ? false : (stoch.length > 1 ? (((stoch[stoch.length - 1].k || stoch[stoch.length - 1].k === 0) && (stoch[stoch.length - 1].d || stoch[stoch.length - 1].d === 0)) ? (stoch[stoch.length - 1].k > stoch[stoch.length - 1].d) : false) : false);
+                const stochBear = stochOS ? false : (stoch.length > 1 ? (((stoch[stoch.length - 1].k || stoch[stoch.length - 1].k === 0) && (stoch[stoch.length - 1].d || stoch[stoch.length - 1].d === 0)) ? (stoch[stoch.length - 1].k < stoch[stoch.length - 1].d) : false) : false);
+
+                // COMPUTE TREND CONFIRMATION AND SUPORTING INDICATORS
+                const trendBull = bullish(csd);
+                const trendBear = bearish(csd);
+                const vwap = VWAP.calculate({ close, high, low, volume });
+                const vwapBull = vwap.length > 0 ? latestRate > vwap[vwap.length - 1] : false;
+                const vwapBear = vwap.length > 0 ? latestRate < vwap[vwap.length - 1] : false;
+                const adl = ADL.calculate({ close, high, low, volume });
+                const adlDir = computeArithmeticDirection(adl);
+                const adlBull = adlDir > 0 && priceDir > 0;
+                const adlBear = adlDir < 0 && priceDir < 0;
+
+
+                console.log("MACD", "BULL", macdBull, "BEAR", macdBear);
+                console.log("PSAR", "BULL", psarBull, "BEAR", psarBear);
+                console.log("STOCH", "BULL", stochBull, "BEAR", stochBear, "OB", stochOB, "OS", stochOS);
+                console.log("PRICE DIR", priceDir);
+                console.log("TREND", "BULL", trendBull, "BEAR", trendBear);
+                console.log("VWAP", "BULL", vwapBull, "BEAR", vwapBear);
+                console.log("ADL", "BULL", adlBull, "BEAR", adlBear);
+
+
+                resolve(null);
             }
             else {
                 Log.flow(`Analysis > ${symbol} > Error > Not enough rows.`, 5);
--- site.js
+++ site.js
@@ -27,6 +27,24 @@ class Site {
 
     static AS_MIN_ROWS = parseInt(process.env.AS_MIN_ROWS || "100");
 
+    static IN_DIRECTION_MAX_LENGTH = parseInt(process.env.IN_DIRECTION_MAX_LENGTH || "10");
+    static IN_BOOLEAN_THRESHOLD_MIN_RATIO = parseFloat(process.env.IN_BOOLEAN_THRESHOLD_MIN_RATIO || "0.5");
+    static IN_MACD_FAST_PERIOD = parseInt(process.env.IN_MACD_FAST_PERIOD || "12") || 12;
+    static IN_MACD_SLOW_PERIOD = parseInt(process.env.IN_MACD_SLOW_PERIOD || "26") || 26;
+    static IN_MACD_SIGNAL_PERIOD = parseInt(process.env.IN_MACD_SIGNAL_PERIOD || "9") || 9;
+    static IN_MA_PERIOD = parseInt(process.env.IN_MA_PERIOD || "10") || 10;
+    static IN_AO_FAST_PERIOD = parseInt(process.env.IN_AO_FAST_PERIOD || "5") || 5;
+    static IN_AO_SLOW_PERIOD = parseInt(process.env.IN_AO_SLOW_PERIOD || "34") || 34;
+    static IN_FI_PERIOD = parseInt(process.env.IN_FI_PERIOD || "14") || 14;
+    static IN_BB_PERIOD = parseInt(process.env.IN_BB_PERIOD || "20") || 20;
+    static IN_BB_STDDEV = parseFloat(process.env.IN_BB_STDDEV || "2") || 2;
+    static IN_PSAR_STEP = parseFloat(process.env.IN_PSAR_STEP || "0.02") || 0.02;
+    static IN_PSAR_MAX = parseFloat(process.env.IN_PSAR_MAX || "0.2") || 0.2;
+    static IN_STOCH_PERIOD = parseInt(process.env.IN_STOCH_PERIOD || "14") || 14;
+    static IN_STOCH_SIGNAL_PERIOD = parseInt(process.env.IN_STOCH_SIGNAL_PERIOD || "3") || 3;
+    static IN_MAX_SIGNAL_HISTORY_LENGTH = parseInt(process.env.IN_MAX_SIGNAL_HISTORY_LENGTH || "5") || "5";
+    static IN_ML_COLLECT_DATA = (process.env.IN_ML_COLLECT_DATA || "").toLowerCase() == "true";
+
     static BG_API_KEY = process.env.BG_API_KEY || "";
     static BG_API_SECRET = process.env.BG_API_SECRET || "";
     static BG_API_PASSPHRASE = process.env.BG_API_PASSPHRASE || "";
