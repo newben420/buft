e3ae5ba7ac840f4bad15f1c4f7c275b7904fdf57
newben420 <wraith@wraith.wraith>
2025-04-18 17:37:46 +0100

    set premise for next tasks - UI notifications, Telegram Integration, Smart close strategy

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use((err, req, res, next) => {
 
 process.on('exit', async (code) => {
     const l = await stopEngine();
+    // TODO - SEND UI NOTIFICATIONS ON EXIT
 });
 
 process.on('SIGINT', async () => {
@@ -54,7 +55,7 @@ process.on('SIGTERM', async () => {
 process.on('uncaughtException', async (err) => {
     Log.flow('Process > Unhandled exception caught.');
     console.log(err);
-    if(Site.EXIT_ON_UNCAUGHT_EXCEPTION){
+    if (Site.EXIT_ON_UNCAUGHT_EXCEPTION) {
         process.exit(0);
     }
 });
@@ -63,19 +64,20 @@ process.on('unhandledRejection', async (err, promise) => {
     Log.flow('Process > Unhandled rejection caught.');
     console.log("Promise:", promise);
     console.log("Reason:", err);
-    if(Site.EXIT_ON_UNHANDLED_REJECTION){
+    if (Site.EXIT_ON_UNHANDLED_REJECTION) {
         process.exit(0);
     }
 });
 
 startEngine().then(started => {
-    Log.flow(`${Site.TITLE} > ${started ? `Started successfully.`: `Failed to start.`}`);
-    if(started){
+    Log.flow(`${Site.TITLE} > ${started ? `Started successfully.` : `Failed to start.`}`);
+    if (started) {
         server.listen(Site.PORT, () => {
             Log.flow(`${Site.TITLE} > ${Site.URL}`);
+            // TODO - SEND UI NOTIFICATIONS ON START
         });
     }
-    else{
+    else {
         process.exit(0);
     }
 }).catch(err => {
